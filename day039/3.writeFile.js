90605ca6756f2faef2e569b568bbe9b6648eefd2
by-white <1142469875@qq.com>
2019-12-23 00:32:40 +0800

    039

---
+++ day039/3.writeFile.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+/* 
+
+fs.writeFile('www/2.txt','567',function(error){
+    if (error) {
+        console.log('失败');
+    }
+})
+fs.open('www/2.txt','r+',function(error,fd){
+    if (error) {
+        console.log('失败')
+    }
+    console.log(fd);
+})
+fs.unlink('www/2.txt','r+',function(err,id){
+    if (err) {
+        return console.error(err);
+    }
+});
+
+*/
+
+//添加文件夹
+fs.mkdir('www/haha',function(){
+
+})
