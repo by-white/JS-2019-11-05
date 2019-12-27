8e6f73b07cdcf439c4d183a9305e99cd073d12e2
by-white <1142469875@qq.com>
2019-12-27 23:42:45 +0800

    043

---
+++ day043/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const obj = {
+    mode: 'development',
+    entry:'./1.js',
+    output:{
+        path:path.resolve(__dirname,'./build'),
+        filename:'build.js'
+    }
+}
+
+
+//导出配置项
+module.exports = obj;
